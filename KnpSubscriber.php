b064a607ce300249be018d2d62e1b005b149bb14
Michael <m@cadot.eu>
2022-09-11 13:36:22 +0200

    page reset sur nouvelle recherche

--- KnpSubscriber.php
+++ KnpSubscriber.php
@@ -9,36 +9,48 @@ use Symfony\Component\HttpKernel\KernelEvents;
 class KnpSubscriber implements EventSubscriberInterface
 {
     private $knparray = ['sort', 'direction', 'page', 'filterValue', 'filterField'];
-
-    public function __construct()
-    {
-    }
+    private $request;
 
     public function onKernelRequest(RequestEvent $event)
     {
-
-        $request = $event->getRequest();
+        //récupération des request
+        $this->request = $event->getRequest();
         //$request->getSession()->clear();
-        if (!$request->hasPreviousSession()) {
+        if (!$this->request->hasPreviousSession()) {
             return;
         }
-        if ($request->isMethod('GET')) {
+
+        if ($this->request->isMethod('GET')) {
+            //reset des pages si on a une nouvelle filterValue
+            if ($this->request->getSession()->get($this->GetName('filterValue'), false) != $this->request->query->get('filterValue', false) && $this->request->query->get('filterValue', false) != false) {
+                $this->request->getSession()->set($this->GetName('page'), 1);
+                $this->request->query->add([$this->GetName('_page') => 1]);
+            }
+            //on liste les champs
             foreach ($this->knparray as $knp) {
-                $field = 'knp_' . $request->attributes->get('_route') . '_' . $knp;
-                if ($val = $request->query->get($knp, false)) {
-                    $request->getSession()->set($field, $val);
+                $field = $this->GetName($knp);
+                //si le get n'est pas false, on enregistre dans la session
+                if ($val = $this->request->query->get($knp, false)) {
+                    $this->request->getSession()->set($field, $val);
                 } else {
-                    if ($request->query->get($knp, 'vide') == '') {
-                        $request->getSession()->remove($field);
+                    //si le get n'est pas false
+                    //suppression des champs
+                    //si le get est "vide" on supprime le champ de la session
+                    if ($this->request->query->get($knp, 'vide') == '') {
+                        $this->request->getSession()->remove($field);
                     }
-                    if ($exval = $request->getSession()->get($field, false)) {
-                        $request->query->add([$knp => $exval]);
+                    //si le get n'est ni "vide" ni false on ajoute au request
+                    if ($exval = $this->request->getSession()->get($field, false)) {
+                        $this->request->query->add([$knp => $exval]);
                     }
                 }
             }
         }
     }
-
+    private function GetName($field)
+    {
+        return 'knp_' . $this->request->attributes->get('_route') . '_' . $field;
+    }
     public static function getSubscribedEvents()
     {
         return [
