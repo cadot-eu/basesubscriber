bba46c130b61475f074df288e00ba161c8692654
michael <m@cadot.eu>
2023-04-05 14:13:05 +0200

    refactor(TwigGlobalSubscriber.php): renommer la méthode 'injectGlobalVariables' en 'injectGlobalVars' pour plus de clarté feat(TwigGlobalSubscriber.php): ajouter la méthode 'params' pour récupérer tous les paramètres de la base de données et les stocker dans un tableau

--- TwigGlobalSubscriber.php
+++ TwigGlobalSubscriber.php
@@ -2,6 +2,7 @@
 
 namespace App\EventSubscriber\base;
 
+use App\Service\base\ArticleHelper;
 use Doctrine\Common\Collections\Criteria;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
@@ -9,6 +10,9 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Twig\Environment;
 use App\Service\base\ToolsHelper;
+use Liip\ImagineBundle\Imagine\Cache\CacheManager;
+use Liip\ImagineBundle\Imagine\Filter\FilterManager;
+use App\Entity\Parametres;
 
 class TwigGlobalSubscriber implements EventSubscriberInterface
 {
@@ -16,15 +20,21 @@ class TwigGlobalSubscriber implements EventSubscriberInterface
 
     private $em;
 
-    public function __construct(Environment $twig, EntityManagerInterface $em)
+    protected $cachemanager;
+
+    protected $filtermanager;
+
+    public function __construct(Environment $twig, EntityManagerInterface $em, CacheManager $cachemanager, FilterManager $filtermanager)
     {
         $this->twig = $twig;
         $this->em = $em;
+        $this->cachemanager = $cachemanager;
+        $this->filtermanager = $filtermanager;
     }
 
     public function injectGlobalVariables()
     {
-        $this->twig->addGlobal('TBparametres', ToolsHelper::params($this->em));
+        $this->twig->addGlobal('TBparametres', $this->params($this->em));
         $this->twig->addGlobal('categories', $this->em->getRepository('App\Entity\Categorie')->findBy(array('deletedAt' => null), array('nom' => 'ASC')));
     }
 
@@ -33,5 +43,22 @@ class TwigGlobalSubscriber implements EventSubscriberInterface
         return [
             KernelEvents::CONTROLLER => 'injectGlobalVariables',
         ];
+    }
+    /**
+     * It returns an array of all the parameters in the database
+     *
+     * @param EntityManagerInterface em The entity manager
+     *
+     * @return An array of all the parameters in the database.
+     */
+    public function params(EntityManagerInterface $em): array
+    {
+        $tab = [];
+        foreach ($this->em->getRepository(Parametres::class)->findAll() as $parametre) {
+            $tab[$parametre->getSlug()] =  //on ajoute les srcset aux images
+                ArticleHelper::imgToSrcset($parametre->getValeur(), $this->cachemanager, $this->filtermanager);
+        }
+
+        return $tab;
     }
 }
